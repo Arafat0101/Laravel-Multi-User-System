3657cde3abd4e1e7c8eebb47350c49afc69bebac
Arafat0101 <arafat2892@gmail.com>
2020-04-22 08:32:52 +0600

    Git Test

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::prefix('admin')->name('admin.')->middleware('can:manage-users')->group(function(){
     Route::resource('/users','UsersController',['except'=>['show','create','store']]);
 });
+
+//Test Git
